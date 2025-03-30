ac7ef293fca6f55fb3e2a2320ecd6a92d0a5e90e
Steffen Busch <steffen.busch@gmail.com>
2025-03-30 15:34:07 +0200

    Remove useless comment

--- user.go
+++ user.go
@@ -79,7 +79,7 @@ func (m *JWTIssuer) loadUsers(filePath string) error {
 		Password        string         `json:"password"`
 		Audience        []string       `json:"audience"`
 		TokenLifetime   *string        `json:"token_lifetime"`
-		TokenValidUntil string         `json:"token_valid_until"` // Add token_valid_until
+		TokenValidUntil string         `json:"token_valid_until"`
 		TOTPSecret      string         `json:"totp_secret"`
 		MetaClaims      map[string]any `json:"meta_claims"`
 	})
